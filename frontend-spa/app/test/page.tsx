a73390d1ab96e4f35a36d8287b50053eeaab5ab5
willi <williamputra04@gmail.com>
2026-02-14 10:51:24 +0700

    spa uses nextjs

---
+++ frontend-spa/app/test/page.tsx
@@ -0,0 +1,11 @@
+import { Button, HStack } from "@chakra-ui/react"
+
+const DemoPage = () => {
+  return (
+    <HStack>
+      <Button>Click Me</Button>
+    </HStack>
+  );
+};
+
+export default DemoPage;
